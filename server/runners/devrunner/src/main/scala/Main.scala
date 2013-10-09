6a87a317ddc71be49a863faad3bfc96b36500fea
Eros Candelaresi <eros@candelaresi.de>
2013-10-09 21:34:39 +0200

    Replaced logging frontend from Typesafe with the one from Grizzled. Prefer the way it can be called withouth the log. or logger. prefix.

--- server/runners/devrunner/src/main/scala/Main.scala
+++ server/runners/devrunner/src/main/scala/Main.scala
@@ -1,9 +1,13 @@
+import grizzled.slf4j.Logging
+
 /**
  * User: Eros Candelaresi <eros@candelaresi.de>
  * Project: Krautmail Server
  * Date: 08.10.2013
  * Time: 22:02
  */
-object Main extends App {
+object Main extends App with Logging {
+  debug("Staring")
   println("Hello world")
+  debug("Ending")
 }
